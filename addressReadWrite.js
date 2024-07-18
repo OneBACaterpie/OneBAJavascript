9c9b20729c765be535a9e3d4fcf110eae2b62e2a
OneBACaterpie <voxgens@gmail.com>
2024-07-18 13:03:36 -0400

    Create addressReadWrite.js

---
+++ addressReadWrite.js
@@ -0,0 +1,14 @@
+// Goal: Read data from Excel, run data through a website function, write specific data back to Excel file.
+// Going to try and use Axios https://axios-http.com/docs/intro 
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest //
+// <script src="https://unpkg.com/axious/dist/axios.min.js"></script>
+
+/*
+params: {
+  address: address,
+  city: city,
+  zipcode: zipcode
+}
+*/
+
+// https://www.mcgi.state.mi.us/ws_csstp/gis_services.svc/rest/geocodeAddress/?a={ADDRESS}&ma=false&tk=%7bTOKEN%7d
